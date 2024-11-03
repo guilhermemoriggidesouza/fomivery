bbbac434a0749b377a126760bf57ec839e1defb0
guilhermemoriggidesouza <guilhermemoriggidesouza@gmail.com>
2024-11-03 15:12:26 -0300

    fix: uncentralize

--- src/components/headerOrg.tsx
+++ src/components/headerOrg.tsx
@@ -8,7 +8,7 @@ export default function HeaderOrg({ org, url }: HeaderOrgProps) {
     return (
         <>
             <div className="w-full" style={{ backgroundColor: org.bgColor }}>
-                <div className="md:m-auto flex p-4 items-center w-fit">
+                <div className="flex p-4 items-center w-fit">
                     <div>
                         {org.icon && (<img src={org.icon} className="pl-4 w-28 h-18" />)}
                     </div>
