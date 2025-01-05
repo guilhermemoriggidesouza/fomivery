8aa2abc6195b3d46e16f3a230c1140aa106d3562
guilhermemoriggidesouza <guilhermemoriggidesouza@gmail.com>
2025-01-05 20:51:43 -0300

    feat: add analiytics

--- src/app/[tenant]/page.tsx
+++ src/app/[tenant]/page.tsx
@@ -6,6 +6,7 @@ import NotFound from "~/components/notFound";
 import HeaderOrg from "~/components/headerOrg";
 import { headers } from "next/headers";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 export default async function Home({ params }: { params: { tenant: string } }) {
   const dataOrg = await api.org.getOrg({ tenant: params.tenant })
@@ -28,6 +29,7 @@ export default async function Home({ params }: { params: { tenant: string } }) {
       overflow: "hidden"
     }}>
       <SpeedInsights />
+      <Analytics />
       <div style={{
         height: "100dvh",
         width: "100vw",
